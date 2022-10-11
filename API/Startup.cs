f3a79a4d2197e3b56b909ac830d580c3d009b036
Daryl Estrada <frdarylestrada@gmail.com>
2022-10-11 12:38:12 +0700

    Walking skeleton - frontend

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,9 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddCors();
+            
  
             AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
             
@@ -45,6 +48,8 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -55,6 +60,8 @@ namespace API
             app.UseHttpsRedirection();
  
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
  
             app.UseAuthorization();
  
