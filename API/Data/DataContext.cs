078bf0017dfca610f902a8227aa0abb2bc7b9e82
Daryl Estrada <frdarylestrada@gmail.com>
2022-10-15 12:55:06 +0700

    Messaging

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -32,7 +32,20 @@ namespace API.Data
                 .WithMany(l => l.LikedByUsers)
                 .HasForeignKey(s => s.LikedUserId)
                 .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Recipient)
+                .WithMany(m => m.MessagesReceived)
+                .OnDelete(DeleteBehavior.Restrict);
+ 
+            builder.Entity<Message>()
+                .HasOne(u => u.Sender)
+                .WithMany(m => m.MessagesSent)
+                .OnDelete(DeleteBehavior.Restrict);
+
         }
+
+        public DbSet<Message> Messages { get; set; }
 
     }
 }
