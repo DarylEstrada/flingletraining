509b9838b4e980fd17d0c072d3b39d79b1391f43
Daryl Estrada <frdarylestrada@gmail.com>
2022-10-17 06:23:20 +0700

    Role management

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -8,6 +8,9 @@ using System.Threading.Tasks;
 using API.Entities;
 using API.Interfaces;
 using Microsoft.IdentityModel.Tokens;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
 
 namespace API.Services
 {
@@ -16,12 +19,16 @@ namespace API.Services
     {
 
         private readonly SymmetricSecurityKey _key;
-        public TokenService(IConfiguration config)
+        private readonly UserManager<AppUser> _userManager;
+        public TokenService(IConfiguration config, UserManager<AppUser> userManager)
         {
-        _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+            _userManager = userManager;
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
         }
 
-        public string CreateToken(AppUser user)
+
+
+        public async Task<string> CreateToken(AppUser user)
         {
             var claims = new List<Claim>
             {
@@ -30,6 +37,9 @@ namespace API.Services
             };
 
  
+            var roles = await _userManager.GetRolesAsync(user);
+            claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+
             var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
  
             var tokenDescriptor = new SecurityTokenDescriptor
